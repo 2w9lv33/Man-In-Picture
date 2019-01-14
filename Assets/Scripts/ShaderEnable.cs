e139b2bb789f757a29516170cb11fe4173e09e1d
2w9lv33 <823271351@qq.com>
2019-01-14 17:53:41 +0800

    add animaiton

---
+++ Assets/Scripts/ShaderEnable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderEnable : MonoBehaviour
+{
+    void OnEnable()
+    {
+        transform.GetComponent<SpriteRenderer>().GetComponent<Renderer>().receiveShadows = true;
+        transform.GetComponent<SpriteRenderer>().GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+}
